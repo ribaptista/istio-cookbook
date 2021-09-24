003912e5fa6d1e4896e3d5c46c0b10dbf23c2817
Ricardo Baptista <ricardogermano85@gmail.com>
2021-09-24 18:04:40 -0300

    Movies service

--- services/movies/index.js
+++ services/movies/index.js
@@ -3,13 +3,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/profile', (req, res) => {
-  res.send({
-    id: 101,
-    name: 'John Doe',
-    birthday: '1985-07-15',
-    email: 'johndoe@email.com'
-  })
+app.get('/top', (req, res) => {
+  res.send([
+    { title: 'Mandalorian' },
+    { title: 'Dark' },
+    { title: 'Black Mirror' },
+  ])
+})
+
+app.get('/recommended/user/101', (req, res) => {
+  res.send([
+    { title: 'Iron Man' },
+    { title: 'Star Wars' },
+    { title: 'Black Mirror' },
+  ])
 })
 
 app.get('/healthcheck', (req, res) => {
